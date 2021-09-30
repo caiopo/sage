f10ad15447db3847df7e36f328452998c154b6ee
caiopo <caiopoliveira@gmail.com>
2021-09-30 20:07:27 -0300

    Add Home drawer

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sage/business/auth_business.dart';
@@ -9,24 +11,68 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(FirebaseAuth.instance.currentUser);
     return Scaffold(
       appBar: AppBar(
         title: SageWordmark(fontSize: 28),
-        actions: [
-          IconButton(
-            onPressed: () async {
+      ),
+      drawer: _Drawer(),
+      body: Container(),
+    );
+  }
+}
+
+class _Drawer extends StatelessWidget {
+  const _Drawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final user = FirebaseAuth.instance.currentUser!;
+
+    final displayName = user.displayName ?? 'User';
+
+    return Drawer(
+      child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              child: Text(
+                displayName
+                    .substring(0, min(2, displayName.length))
+                    .toUpperCase(),
+                style: theme.textTheme.bodyText1?.copyWith(fontSize: 18),
+              ),
+              backgroundColor: theme.colorScheme.secondary,
+              foregroundColor: theme.colorScheme.onSecondary,
+              foregroundImage:
+                  user.photoURL != null ? NetworkImage(user.photoURL!) : null,
+            ),
+            accountEmail: Text(user.email ?? ''),
+            accountName: Text(displayName),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile'),
+            onTap: () async {},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () async {},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Sign out'),
+            onTap: () async {
               await signOut();
               Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(builder: (_) => LoginPage()),
               );
             },
-            icon: Icon(Icons.exit_to_app),
           ),
         ],
       ),
-      body: Container(),
     );
   }
 }
