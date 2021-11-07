61183ea59e9e150b186ff0c9717f716042d50ed3
caiopo <caiopoliveira@gmail.com>
2021-11-07 14:37:22 -0300

    Create project

---
+++ test/sage_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule SageWeb.ErrorViewTest do
+  use SageWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(SageWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(SageWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
