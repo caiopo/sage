b6c5a142d4d5dd4aa986f51b73bf2ae3c944a9d3
caiopo <caiopoliveira@gmail.com>
2022-09-16 17:05:27 -0300

    Fix deployment issues

--- config/runtime.exs
+++ config/runtime.exs
@@ -21,17 +21,17 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  database_path =
-    System.get_env("DATABASE_PATH") ||
-      System.get_env("DATABASE_URL") ||
+  database_url =
+    System.get_env("DATABASE_URL") ||
       raise """
-      environment variable DATABASE_PATH is missing.
+      environment variable DATABASE_URL is missing.
       For example: /etc/sage/sage.db
       """
 
   config :sage, Sage.Repo,
-    database: database_path,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5")
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5"),
+    socket_options: [:inet6]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
