768f24f86b8cab561104f39a24222398a62b3f7e
caiopo <caiopoliveira@gmail.com>
2022-09-17 19:28:08 -0300

    Improve admin dashboard

--- lib/sage_web/admin/accounts_admin.ex
+++ lib/sage_web/admin/accounts_admin.ex
@@ -10,25 +10,6 @@ defmodule SageWeb.Admin.AccountsAdmin do
         icon: "user",
         order: 1,
         width: 6
-      },
-      %{
-        type: "progress",
-        title: "Pancakes",
-        content: "Customer Satisfaction",
-        percentage: 89,
-        order: 3,
-        width: 6
-      },
-      %{
-        type: "chart",
-        title: "This week's sales",
-        order: 8,
-        width: 12,
-        content: %{
-          x: ["Mon", "Tue", "Wed", "Thu", "Today"],
-          y: [150, 230, 75, 240, 290],
-          y_title: "USD"
-        }
       }
     ]
   end
--- lib/sage_web/admin/config.ex
+++ lib/sage_web/admin/config.ex
@@ -2,20 +2,11 @@ defmodule SageWeb.Admin.Config do
   def create_resources(_conn) do
     [
       accounts: [
-        name: "Users",
+        name: "Accounts",
         resources: [
           users: [schema: Sage.Accounts.User, admin: SageWeb.Admin.AccountsAdmin]
-          # comment: [schema: MyApp.Blog.Comment],
-          # tag: [schema: MyApp.Blog.Tag]
         ]
       ]
-      # inventory: [
-      #   name: "Inventory",
-      #   resources: [
-      #     category: [schema: MyApp.Products.Category, admin: MyApp.Products.CategoryAdmin],
-      #     product: [schema: MyApp.Products.Product, admin: MyApp.Products.ProductAdmin]
-      #   ]
-      # ]
     ]
   end
 end
